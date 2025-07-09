5d19cbcb89df506bb579a25f350dff916d65c167
Samir Radjabov <samirradzabov367@gmail.com>
2025-07-09 16:00:25 +0500

    ваше сообщение коммита

--- muxatech/urls.py
+++ muxatech/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from app import views
+from app import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path('', views.home, name='home'),  
 ]
